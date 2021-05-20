7e5161693d8c94b4ed6378f55018e9fd9290d35a
Andrey Khomutov <andrey.khomutov@t-systems.com>
2021-05-20 10:23:06 +0300

    Initial commit

---
+++ src/leetcode/arrays/insertingItemsIntoAnArray/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.arrays.insertingItemsIntoAnArray;
+
+//https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/
+public class Solution {
+
+    public void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                for (int j = arr.length - 1; j > i + 1; j--) {
+                    arr[j] = arr[j - 1];
+                }
+                if ((i + 1) < arr.length) {
+                    arr[i + 1] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+}
