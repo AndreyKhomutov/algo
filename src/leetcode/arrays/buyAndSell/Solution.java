f27a83208181b65fc0b987e1e46b8b8d46995805
Andrey Khomutov <andrey.khomutov@t-systems.com>
2021-09-17 00:46:49 +0300

    buyAndSell init

---
+++ src/leetcode/arrays/buyAndSell/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.arrays.buyAndSell;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+public class Solution {
+    public static void main(String[] args) {
+        int[] input = {7,1,5,3,6,4};
+        System.out.println(maxProfit(input));
+    }
+
+    public static int maxProfit(int[] prices) {
+        return 0;
+    }
+}
