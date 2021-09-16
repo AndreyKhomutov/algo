c8129d5aac68402c5dde64019bdfa2f2a8370cbe
Andrey Khomutov <andrey.khomutov@t-systems.com>
2021-09-17 00:57:05 +0300

    buyAndSell done)

--- src/leetcode/arrays/buyAndSell/Solution.java
+++ src/leetcode/arrays/buyAndSell/Solution.java
@@ -3,11 +3,18 @@ package leetcode.arrays.buyAndSell;
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
 public class Solution {
     public static void main(String[] args) {
-        int[] input = {7,1,5,3,6,4};
+//        int[] input = {7,1,5,3,6,4};//7
+        int[] input = {1,2,3,4,5};//4
         System.out.println(maxProfit(input));
     }
 
     public static int maxProfit(int[] prices) {
-        return 0;
+        int result = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i]>prices[i-1]){
+                result+=prices[i]-prices[i-1];
+            }
+        }
+        return result;
     }
 }
